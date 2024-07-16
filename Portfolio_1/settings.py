ff6039d03afbcc5cf6e0ae35a936a3d26d50a648
Lysden97 <lysenko.denys97@gmail.com>
2024-07-16 19:15:01 +0200

    Konfiguracja projektu Django

--- Portfolio_1/settings.py
+++ Portfolio_1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "website.apps.WebsiteConfig"
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'Portfolio_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'charity-donation',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
