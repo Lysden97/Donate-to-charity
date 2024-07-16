479e1151f54494031c5b343562af282e43b60c58
Lysden97 <lysenko.denys97@gmail.com>
2024-07-17 00:27:14 +0200

    Dynamiczne statystyki

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import CreateView
 
+from website.models import Donation, Institution
+
 
 class RegisterView(View):
     def get(self, request):
@@ -11,6 +13,19 @@ class RegisterView(View):
 class LoginView(View):
     def get(self, request):
         return render(request, 'login.html')
+
+
+class IndexView(View):
+    def get(self, request):
+        all_donations = Donation.objects.all()
+        all_bags = sum(donation.quantity for donation in all_donations)
+        supported_organizations = Institution.objects.count()
+
+        context = {
+            'all_bags': all_bags,
+            'supported_organizations': supported_organizations,
+        }
+        return render(request, 'index.html', context)
 
 
 class AddDonationView(View):
