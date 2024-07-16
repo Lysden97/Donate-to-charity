ce22ecd50f14777fb60fd914bde5e275d1afdc56
Lysden97 <lysenko.denys97@gmail.com>
2024-07-16 22:52:01 +0200

    Przygotowanie szablonow

--- website/urls.py
+++ website/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+
+from django.urls import path
+from website import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('website/', include('website.urls')),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('add_donation/', views.AddDonationView.as_view(), name='add_donation'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form.html')
