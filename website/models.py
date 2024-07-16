f4eb700072e85b3f4aa004fd3e7332ad5d4dd0fc
Lysden97 <lysenko.denys97@gmail.com>
2024-07-16 23:18:29 +0200

    Model Institution

--- website/models.py
+++ website/models.py
@@ -5,3 +5,18 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Institution(models.Model):
+    CHOICES = (
+        (1, 'fundacja'),
+        (2, 'organizacja pozarządowa'),
+        (3, 'zbiórka lokalna'),
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.IntegerField(choices=CHOICES, default=1)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return f'{self.name}'
